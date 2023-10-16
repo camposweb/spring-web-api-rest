c176a9e420b48c1fa1cf6b88f6beb93ece3250c7
Leandro Campos <camposweb@gmail.com>
2023-10-16 19:26:11 -0300

    initial commit

---
+++ src/main/java/dio/springwebapirest/SpringWebApiRestApplication.java
@@ -0,0 +1,13 @@
+package dio.springwebapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/springwebapirest/SpringWebApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springwebapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
